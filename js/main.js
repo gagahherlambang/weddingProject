431e23ca448ee3ba5def8fa7372c6701001a8aa2
Developer Gagah <developer.balitower@gmail.com>
2017-12-12 15:28:27 +0700

    push change time into 10 am

--- js/main.js
+++ js/main.js
@@ -137,7 +137,7 @@
 	};
 
 	// Set the date we're counting down to
-	var countDownDate = new Date("Jan 27, 2018 11:00:00").getTime();
+	var countDownDate = new Date("Jan 27, 2018 10:00:00").getTime();
 
 	// Update the count down every 1 second
 	var x = setInterval(function() {
